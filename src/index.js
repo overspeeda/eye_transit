7673886adf7f08c28a8b7c8d50cb6141ee2de1bd
overspeeda <92965379+overspeeda@users.noreply.github.com>
2023-03-11 22:52:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_eTOtg0_fQnBOz4QmAXM8SlHRFAcvhjY",
-  authDomain: "bus-track-361306.firebaseapp.com",
-  databaseURL: "https://bus-track-361306-default-rtdb.firebaseio.com",
-  projectId: "bus-track-361306",
-  storageBucket: "bus-track-361306.appspot.com",
-  messagingSenderId: "745565483285",
-  appId: "1:745565483285:web:debb0cf39cf5943a6df91c",
-  measurementId: "G-G9RDY4EEQV"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
